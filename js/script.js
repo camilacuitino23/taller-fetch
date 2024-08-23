157573ccacb25f22e0451b504e82a98d9c876e51
camilacuitino23 <camilacuitino23@gmail.com>
2024-08-23 02:59:31 -0300

    fetch data

--- js/script.js
+++ js/script.js
@@ -19,3 +19,13 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+
+fetch(DATA_URL).then(respuesta => {
+
+return respuesta.json(); 
+
+}) .then(data => {
+
+showData(data.students);
+
+});
